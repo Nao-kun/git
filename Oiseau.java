041bb3e1664444efc9fea3031c6599cdecfde04c
Nao-kun <nao.aldzero@gmail.com>
2023-09-23 00:32:31 +0200

    Ajout de l'héritage

--- Oiseau.java
+++ Oiseau.java
@@ -1,31 +1,12 @@
-public class Oiseau {
-    private String nom;
-    private int age;
+public class Oiseau extends Animal {
     private boolean vol;
 
     public Oiseau() {
     }
 
     public Oiseau(String nom, int age, boolean vol) {
-        this.nom = nom;
-        this.age = age;
+        super(nom, age);
         this.vol = vol;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
     }
 
     public boolean isVol() {
